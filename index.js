2c9b323ce51e774a5a350ea6f0893f917a3e7493
Jesus Larios <sau30jas@gmail.com>
2022-04-27 15:21:43 -0500

    Agregando prueba de MC

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const Ajolonauta = require('./app/Ajolonauta')
+const MissionCommander = require('./app/missionCommander');
+const carlo = new MissionCommander("Carlo")
+console.log(carlo.name)
 const woopa = new Ajolonauta ("Woopa")
 console.log(woopa.name)
